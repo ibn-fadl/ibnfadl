549b285e2e1ece6079f9df699f877b7c3c126fc0
Ardian <ardian@ibnfadl.com>
2025-12-22 01:19:34 +0700

    feat: dark mode site logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,5 +6,21 @@
 
 <svelte:head>
 	<title>Ardian's Personal Site</title>
+	<meta name="description" content="Personal website of Ardian" />
+
+	<!-- Facebook Meta Tags -->
+	<meta property="og:url" content="https://ibnfadl.com" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Ardian's Personal Site" />
+	<meta property="og:description" content="Personal website of Ardian" />
+	<meta property="og:image" content="https://ibnfadl.com/og-image.jpg" />
+
+	<!-- Twitter Meta Tags -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta property="twitter:domain" content="ibnfadl.com" />
+	<meta property="twitter:url" content="https://ibnfadl.com" />
+	<meta name="twitter:title" content="Ardian's Personal Site" />
+	<meta name="twitter:description" content="Personal website of Ardian" />
+	<meta name="twitter:image" content="https://ibnfadl.com/og-image.jpg" />
 </svelte:head>
 {@render children()}
