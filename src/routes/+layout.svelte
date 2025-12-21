2b364e70138de0bad6d5c1140d269023828f9e45
Ardian <ardian@ibnfadl.com>
2025-12-21 14:55:06 +0700

    fix: site logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/icon.svg';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<title>Ardian's Personal Site</title>
-	<link rel="icon" href={favicon} />
 </svelte:head>
 {@render children()}
