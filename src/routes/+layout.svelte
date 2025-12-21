32cc65258480d0cf48e845181a1235616ce8e79e
Ardian <ardian@ibnfadl.com>
2025-12-21 14:01:18 +0700

    feat: site logo and title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/icon.svg';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<title>Ardian's Personal Site</title>
+	<link rel="icon" href={favicon} />
+</svelte:head>
 {@render children()}
